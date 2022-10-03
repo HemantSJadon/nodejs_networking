1aeae8e3b0ff31df5759fd283087217509c5ea63
jadonhs185 <h.s.jadon185@gmail.com>
2022-10-03 13:58:36 +0530

    add tcp server

---
+++ tcp.js
@@ -0,0 +1,23 @@
+import * as net from "net";
+
+const server = net.createServer(socket => {
+    console.log("client connected");
+    socket.on("end", () => {
+        console.log("client disconnected");
+    });
+    socket.on("data", data => {
+        console.log(data.toString());
+    });
+    socket.pipe(socket).pipe(socket); // this pipe function does something strange. Check the client when you are connecting
+    socket.write("Hello.");
+});
+
+server.listen(8080, () => {
+    console.log("server bound.");
+});
+
+/*To connect to this server, 
+1. open command prompt
+2. telnet localhost 8080
+
+*/
